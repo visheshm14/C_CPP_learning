3946f5c4a6a5ce655740c8fbb377c646b7871edb
Vishesh Mishra <visheshmishra149@gmail.com>
2026-04-21 19:59:57 +0530

    Add files via upload

---
+++ p6.c
@@ -0,0 +1,13 @@
+// 1 to number
+#include <stdio.h>
+void main()
+{
+    int x;
+    printf("enter any no ");
+    scanf("%d", &x);
+
+    for (int i = 0; i <= x; i++)
+    {
+        printf("%d\n", i);
+    }
+}
