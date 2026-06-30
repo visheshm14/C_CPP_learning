6599f5141c50ce601c949c35705388b45b9e49d0
Vishesh Mishra <visheshmishra149@gmail.com>
2026-06-30 09:54:48 +0530

    Add files via upload

---
+++ p35.c
@@ -0,0 +1,16 @@
+// pattern
+#include <stdio.h>
+void main()
+{
+  
+
+    int i, j;
+    for (i =1; i <= 5; i++)
+    {
+        for (j =1 ; j <= i; j++)
+           {
+             printf("%d\t",(i*(i-1))/2+j);
+           } 
+        printf("\n");
+    }
+}
