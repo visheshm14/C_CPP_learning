0b59613d2fd1c129a382ee030674840ab8cc257e
Vishesh Mishra <visheshmishra149@gmail.com>
2026-07-03 08:59:20 +0530

    Add files via upload

---
+++ p38.c
@@ -0,0 +1,19 @@
+// pattern
+#include <stdio.h>
+void main()
+{
+  
+
+    int i, j;
+    for (i =1; i <= 5; i++)
+    {
+        for (j =1 ; j <= i; j++)
+           {
+             if(i%2==0)
+             printf("#");
+             else
+             printf("*");
+           } 
+        printf("\n");
+    }
+}
