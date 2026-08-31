4e2b4e1c26a80f2bfaa20733b218c18a4a768c48
Vishesh Mishra <visheshmishra149@gmail.com>
2026-08-31 14:30:32 +0530

    Add files via upload

---
+++ p69.c
@@ -0,0 +1,46 @@
+// multiplication of 2d array
+#include<stdio.h>
+void main()
+{
+    int a[2][2],b[2][2],c[2][2];
+  
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            c[i][j] = 0;
+        }
+    }
+
+    
+    printf("enter elements of the first matrix:\n");
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    
+    printf("enter elements of the second matrix:\n");
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    
+    for (int i = 0; i < 2; i++) {  
+        for (int j = 0; j < 2; j++) {  
+            for (int k = 0; k < 2; k++) { 
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    
+    printf("Product of the matrices:\n");
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("%d ", c[i][j]);
+        }
+        printf("\n");
+    }
+}
