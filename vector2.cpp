d5547ec5679bc951767d995d7b07935401786c5f
Vishesh Mishra <visheshmishra149@gmail.com>
2026-09-17 23:26:03 +0530

    Add files via upload

---
+++ vector2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<int> vec;
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+    vec.push_back(4);
+    vec.push_back(5);
+    cout<<vec.size()<<endl;
+    cout<<vec.capacity();
+
+    return 0;
+}
